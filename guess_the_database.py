722917670c7a4b6e81a4a77cd3c9106b2945265d
elaine <elainemuhombe.codingal@gmail.com>
2025-04-18 00:41:18 +0300

    chore: Modify answer

--- guess_the_database.py
+++ guess_the_database.py
@@ -1,7 +1,7 @@
 print("What is the type of above given database -\n1) Relational Database\n2) Non-Relational Database")
 answer = int(input("Enter your guess here... "))
 
-if answer == 2:
+if answer == 1:
     print("You guessed it right!")
 else:
     print("Unfortunately your guess was wrong.")
